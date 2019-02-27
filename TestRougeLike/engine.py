7f5e47bc31d4c7510a2d63c5852793da81fea335
Josiah Pippin <nothirdmolars@yahoo.com>
2019-02-27 00:37:53 -0500

    Add files via upload

---
+++ TestRougeLike/engine.py
@@ -0,0 +1,56 @@
+import libtcodpy as libtcod
+
+#import key handling function
+from input_handlers import handle_keys
+
+def main():
+    screen_width = 80
+    screen_height = 50
+
+    # Set initial player location
+    player_x = int(screen_width/2);
+    player_y = int(screen_height/2);
+
+    # Load font
+    libtcod.console_set_custom_font('arial10x10.png', libtcod.FONT_TYPE_GREYSCALE | libtcod.FONT_LAYOUT_TCOD)
+
+    libtcod.console_init_root(screen_width, screen_height, 'Python Rougelike First Try', False)
+
+    # create console
+    con = libtcod.console_new(screen_width, screen_height)
+
+    key = libtcod.Key()
+    mouse = libtcod.Mouse()
+
+    while not libtcod.console_is_window_closed():
+        libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)
+
+        libtcod.console_set_default_foreground(con, libtcod.white)
+        libtcod.console_put_char(con, player_x, player_y, '@', libtcod.BKGND_NONE)
+        libtcod.console_blit(con, 0, 0, screen_width, screen_height, 0, 0, 0)
+
+        libtcod.console_flush()
+
+        # Right over previous player position with a space
+        libtcod.console_put_char(con, player_x, player_y, ' ', libtcod.BKGND_NONE)
+
+        action = handle_keys(key)
+
+        move = action.get('move')
+        exit = action.get('exit')
+        fullscreen = action.get('fullscreen')
+
+        if move:
+            dx, dy = move
+            player_x += dx
+            player_y += dy
+
+        if exit:
+            return True
+
+        if fullscreen:
+            libtcod.console_set_fullscreen(not libtcod.console_is_fullscreen())
+
+
+if __name__ == '__main__':
+     main()
