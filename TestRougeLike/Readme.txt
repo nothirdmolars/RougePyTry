3d4f50ca1bd331388c27b7842e0057f4caab2ac7
Josiah Pippin <nothirdmolars@yahoo.com>
2019-03-14 12:50:10 -0400

    Add files via upload

--- TestRougeLike/Readme.txt
+++ TestRougeLike/Readme.txt
@@ -1,1 +1,1 @@
-This is just a first try at both Python and creating a rougelike, using the so far excellent tutorial at http://rogueliketutorials.com/libtcod/
+This is just a first try at both Python and creating a rougelike, using the so far excellent tutorial at http://rogueliketutorials.com/libtcod/1
---
+++ TestRougeLike/map_objects/game_map.py
@@ -0,0 +1,28 @@
+from map_objects.tile import Tile
+
+
+class GameMap:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.tiles = self.initialize_tiles()
+    
+    # test version of initialize tiles with dummy data
+    def initialize_tiles(self):
+        tiles = [[Tile(False) for y in range(self.height)] for x in range(self.width)]
+
+        tiles[30][22].blocked = True
+        tiles[30][22].block_sight = True
+        tiles[31][22].blocked = True
+        tiles[31][22].block_sight = True
+        tiles[32][22].blocked = True
+        tiles[32][22].block_sight = True
+
+        return tiles
+
+    # evaluate if a destination tile is blocking
+    def is_blocked(self, x, y):
+        if self.tiles[x][y].blocked:
+            return True
+
+        return False
